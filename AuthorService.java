2c3d6c6a85a3c80e5ea398d3b20bee10fab80eaf
Mohammed Sardar <mdsardar.javadeveloper@gmail.com>
2018-11-23 17:18:29 +0530

    JSON Return with Query Param

--- AuthorService.java
+++ AuthorService.java
@@ -8,24 +8,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/AuthorService")
 public class AuthorService {
 	@GET
 	@Path("/authors")	
 	@Produces(MediaType.APPLICATION_JSON)
-	public List getTrackInJSON() {
-		List listPerson = new ArrayList();
+	public Response getTrackInJSON(@QueryParam("from") int from) {
+		/*List listPerson = new ArrayList();
 		Person p1 = new Person();
 		p1.setId(1);;
 		p1.setName("name1");
 		Person p2 = new Person();
-		p2.setId(2);
+		p2.setId(from);
 		p2.setName("name2");
 		listPerson.add(p1);
 		listPerson.add(p2);
-		return listPerson;
+		return listPerson;*/
+		return Response
+				   .status(200)
+				   .entity("getUsers is called, from : " + from).build();
 	}
 	
+	
+	/*public Response getUsers(
+			@QueryParam("from") int from,
+			@QueryParam("to") int to,
+			@QueryParam("orderBy") List<String> orderBy) {
+
+			return Response
+			   .status(200)
+			   .entity("getUsers is called, from : " + from + ", to : " + to
+				+ ", orderBy" + orderBy.toString()).build();
+
+		}*/
 	
 }
